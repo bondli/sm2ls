9103b2c38a2124993c0fcee2f6c00cecc384808a
pixi <hongbang.lhb@alibaba-inc.com>
2015-03-19 14:35:32 +0800

    bugfix

---
+++ example/0.0.2/testm/index.js
@@ -0,0 +1,48 @@
+define("testm/index", [ "bower_components/admix-ui/build/console/console" ], function(require, exports, module) {
+    var console = require("bower_components/admix-ui/build/console/console");
+
+    console.log('test m');
+
+});
+
+define("bower_components/admix-ui/build/console/console", [], function(require, exports, module) {
+    if (window.location.href.indexOf("admix-ui") === -1) return window.console;
+    var $console = $('<div class="admix-ui-console"><h3>自定义控制台</h3><span>关闭</span></div>').appendTo($("body"));
+    $console.css({
+        position: "fixed",
+        bottom: "0",
+        left: "0",
+        right: "0",
+        background: "rgb(241, 237, 237)",
+        padding: "5px",
+        "font-size": "12px",
+        color: "rgb(51, 51, 51)",
+        "z-index": "9999",
+        "max-height": "30%",
+        overflow: "auto"
+    });
+    $console.find("span").css({
+        color: "#fff",
+        position: "absolute",
+        right: "5px",
+        top: "5px",
+        display: "block",
+        borderRadius: "5px",
+        background: "#ff0000",
+        padding: "5px"
+    }).tap(function() {
+        $console.hide();
+    });
+    return {
+        log: function() {
+            window.console["log"].apply(window.console, arguments);
+            var str = '<div style="margin-bottom:5px;">';
+            for (var i in arguments) {
+                str += arguments[i] + " ";
+            }
+            str += "</div>";
+            $console.append(str);
+        }
+    };
+    return _console;
+});
--- src/seajs-localstorage.js
+++ src/seajs-localstorage.js
@@ -1,24 +1,70 @@
 (function () {
 
-    var isOnline = ~window.location.hostname.indexOf('h5.m.') ? true : false;
+    //是否线上模式
+    var isEnable = ~window.location.hostname.indexOf('h5.m.') ? true : false;
 
-    if(!('localStorage' in window) || !('slice' in [])) {
-        return ;
+    //当前的页面URL
+    var curHref = window.location.href;
+    var curPage = curHref;
+    if(curHref.indexOf('?')>-1){
+        curPage = curHref.substring(0, curHref.indexOf('?'));
     }
 
+    //是否执行了版本检查
+    var isChecked = false;
+
+    //是否需要更新版本
+    var isUpdated = false;
+
+    //当前的版本号
+    var curVersion;
+
+    //之前的版本号
+    var lastVersion = getLastVersion();
+
+    //从来就没有设置到版本号
+    if(!lastVersion){
+        isUpdated = true;
+    }
+
+    if(!('localStorage' in window) || !('slice' in [])) {
+        return;
+    }
+
+    /**
+     * 保存到localstorage
+     * @param  {[type]} uri     [description]
+     * @param  {[type]} dataUri [description]
+     * @return {[type]}         [description]
+     */
     function save(uri, dataUri) {
         //防止重复保存
-        var d = localStorage.getItem('seajscache<' + uri + '>');
+        var d = localStorage.getItem('seajsmodule<' + uri + '>');
         if(d && d.length > 0) {
             return false;
         }
-        dataUri = dataUri.toString();
-        localStorage.setItem('seajscache<' + uri + '>', encodeURI('data:text/javascript;charset=utf-8,define(' + dataUri + ');'));
-        return true;
+
+        try{
+            dataUri = dataUri.toString();
+            localStorage.setItem(
+                'seajsmodule<' + uri + '>',
+                encodeURI('data:text/javascript;charset=utf-8,define(' + dataUri + ');')
+            );
+            return true;
+        }
+        catch(oException){
+            //存储失败了就清除所有的
+            localStorage.clear();
+        }
     }
 
+    /**
+     * 从localstorage取数据
+     * @param  {[type]} uri [description]
+     * @return {[type]}     [description]
+     */
     function load(uri) {
-        var dataUri = localStorage.getItem('seajscache<' + uri + '>');
+        var dataUri = localStorage.getItem('seajsmodule<' + uri + '>');
         if(dataUri && dataUri.length > 0) {
             return dataUri;
         } else {
@@ -26,24 +72,65 @@
         }
     }
 
-    function getLastVersion() {
-        return localStorage.getItem('pageversion') || '';
+    /**
+     * 从localstorage删除缓存
+     * @param  {[type]} uri [description]
+     * @return {[type]}     [description]
+     */
+    function del(uri) {
+        //如果版本更新了，需要删除原来的缓存
+        if( isUpdated ) {
+            var lastKey = uri.replace(curVersion, lastVersion);
+            localStorage.removeItem('seajsmodule<' + lastKey + '>');
+        }
     }
 
-    function latestSource(uri) {
-        // 检测本地缓存是否最新
-        var curVersion = uri.match(/\d\.\d\.\d/);
-        if(curVersion && curVersion.length){
-            curVersion = curVersion[0];
-            var lastVersion = getLastVersion();
-            if(lastVersion && lastVersion != curVersion){
-                //删除原来的localstorage
-                var lastKey = uri.replace(curVersion, lastVersion);
-                localStorage.removeItem('seajscache<'+ lastKey +'>');
-            }
-            //设置当前的版本
-            localStorage.setItem('pageversion', curVersion);
+    /**
+     * 获取上次的页面版本
+     * @return {[type]} [description]
+     */
+    function getLastVersion() {
+        return localStorage.getItem('seajspage<'+ curPage + '>') || '';
+    }
+
+    /**
+     * 设置当前的版本号
+     * @return {[type]} [description]
+     */
+    function saveVersion(v) {
+        if( typeof(window.enableSM2LS) != 'undefined' ){
+            isEnable = window.enableSM2LS;
         }
+        if(isEnable){
+            localStorage.setItem('seajspage<'+ curPage + '>', v);
+        }
+    }
+
+    /**
+     * 检测本地缓存是否最新
+     * @param  {[type]} uri [description]
+     * @return {[type]}     [description]
+     */
+    function latestSource(uri) {
+        //没有检查就做一次检查
+        if( !isChecked ){
+            var cv = uri.match(/\d\.\d\.\d/);
+            if(cv && cv.length){
+                curVersion = cv[0];
+
+                //设置过版本号，但不是最新的
+                if(lastVersion && lastVersion != curVersion){
+                    isUpdated = true;
+                }
+            }
+
+            //设置当前的版本
+            if( isUpdated ){
+                saveVersion(curVersion);
+            }
+
+        }
+        isChecked = true;
 
         return load(uri);
     }
@@ -62,15 +149,15 @@
 
     var oldExec = seajs.Module.prototype.exec;
     seajs.Module.prototype.exec = function () {
-        //是否保存到localstorage的条件判断
-        var enable = isOnline;
         if( typeof(window.enableSM2LS) != 'undefined' ){
-            enable = window.enableSM2LS;
+            isEnable = window.enableSM2LS;
         }
-        
-        if(enable && this.uri && this.factory && !/\.css(?:\?|$)/i.test(this.uri)) {
-            save(this.uri, this.factory.toString().replace(/\r/, '').replace(/\n/, ''));
+
+        if(isEnable && this.uri && this.factory && !/\.css(?:\?|$)/i.test(this.uri)) {
+            del(this.uri);
+            save(this.uri, this.factory.toString());
         }
+
         return oldExec.apply(this, Array.prototype.slice.call(arguments));
     }
 
